de9a6c95afd197856f2412ed24499e61f3cbc079
derekchancts <derekchancts@yahoo.com.hk>
2021-04-18 03:17:43 +0800

    added revalidation to homepage and to slug

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,6 @@
-// import { createClient } from 'contentful';
-// import RecipeCard from '../components/RecipeCard'
-
-
-// export default function Recipes({ recipes }) {
-//   console.log(recipes)
-
-//   return (
-//     <div className="recipe-list">
-//       {recipes && recipes.map(recipe => (
-//         <RecipeCard key={recipe.sys.id} recipe={recipe} />
-//       ))}
-
-//       <style jsx>{`
-//         .recipe-list{
-//           display: grid;
-//           grid-template-columns: 1fr 1fr;
-//           grid-gap: 60px 60px;
-//         }
-//       `}</style>
-//     </div>
-//   )
-// };
-
-
 
 import RecipeCard from '../components/RecipeCard.js'
 import { createClient } from 'contentful'
-
-
-export async function getStaticProps() {
-
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
-
-  const res = await client.getEntries({ content_type: 'receipe' });
-
-  return {
-    props: {
-      recipes: res.items
-    }
-  }
-  
-};
-
-
-
-
-// export default function Recipes({ recipes }) {
-//   console.log(recipes)
-
-//   return (
-//     <div className="recipe-list">
-//       {recipes.map(recipe => (
-//         <RecipeCard key={recipe.sys.id} recipe={recipe} />
-//       ))}
-
-//       <style jsx>{`
-//         .recipe-list {
-//           display: grid;
-//           grid-template-columns: 1fr 1fr;
-//           grid-gap: 20px 60px;
-//         }
-//       `}</style>
-//     </div>
-//   )
-// }
 
 
 export default function Recipes({ recipes }) {
@@ -88,3 +22,28 @@ export default function Recipes({ recipes }) {
     </div>
   )
 }
+
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: 'receipe' });
+
+  return {
+    props: {
+      recipes: res.items,
+      revalidate: 1  // will revalidate/refresh the page (if it has any updated data) after x seconds 
+                     // after a user visits the page. so, subsequent users will see the updated page details 
+    }
+  }
+  
+};
+
+
+
+
+
